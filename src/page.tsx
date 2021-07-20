4697389d9502e43c7da0fe783ab5c3ce72ee4450
Lonnie Liu <liulonnie@gmail.com>
2021-07-20 08:48:00 -0700

    -

---
+++ src/page.tsx
@@ -0,0 +1,73 @@
+// Copyright (C) 2021  Shanhu Tech Inc.
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Affero General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License
+// for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import * as React from 'react' // for tsx
+
+export class Prop {
+    title: string
+    css: string
+}
+
+function cssLink(css: string) {
+    return <link rel="styleSheet" type="text/css" href={css} key={css} />
+}
+
+export class Page {
+    name: string
+    title: string = 'Lonnie'
+    scripts: string[] = []
+    css: string = '/style.css'
+    cssExtra: string[] = []
+    body: JSX.Element = null
+    bodyClass: string = null
+    bodyFunc?: (() => JSX.Element)
+
+    viewport: JSX.Element = <meta name="viewport"
+        content="width=device-width, initial-scale=1" />
+
+    constructor(name: string, prop: Prop) {
+        this.name = name
+        this.title = prop.title
+        this.css = prop.css
+        this.title = prop.title
+    }
+
+    renderBody() {
+        if (this.bodyFunc) { return this.bodyFunc() }
+        return this.body
+    }
+
+    render() {
+        let cssLinks = [cssLink(this.css)]
+        for (let css of this.cssExtra) {
+            cssLinks.push(cssLink(css))
+        }
+
+        let bodyProps = this.bodyClass ? { className: this.bodyClass } : {}
+
+        return <html lang="en">
+            <head>
+                <meta charSet="UTF-8" />
+                <title>{this.title}</title>
+                {cssLinks}
+                {this.viewport}
+            </head>
+            <body {...bodyProps}>
+                {this.renderBody()}
+                {this.scripts.map(f => <script src={f} key={f} />)}
+            </body>
+        </html>
+    }
+}
---
+++ src/writer.tsx
@@ -0,0 +1,73 @@
+// Copyright (C) 2021  Shanhu Tech Inc.
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Affero General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License
+// for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import * as ReactDOMServer from 'react-dom/server'
+
+import * as fs from 'fs'
+import * as path from 'path'
+import * as mkdirp from 'mkdirp'
+import * as page from './page'
+
+export class TemplateWriter {
+    dir: string
+    dirMade: boolean
+
+    constructor(dir: string) {
+        this.dir = dir
+        this.dirMade = false
+    }
+
+    mkdir() {
+        if (this.dirMade) {
+            return
+        }
+        mkdirp.sync(this.dir)
+        this.dirMade = true
+    }
+
+    write(f: string, dom: JSX.Element) {
+        this.mkdir()
+        let p = path.join(this.dir, f)
+        console.log(p)
+        let s = '<!doctype html>\n' + ReactDOMServer.renderToStaticMarkup(dom)
+        fs.writeFileSync(p, s)
+    }
+
+    writePage(p: page.Page) {
+        this.write(p.name + '.html', p.render())
+    }
+
+    writePageFunc(f: (() => page.Page)) {
+        this.writePage(f())
+    }
+
+    writePageFuncs(funcs: (() => page.Page)[]) {
+        for (let f of funcs) {
+            this.writePageFunc(f)
+        }
+    }
+}
+
+export function writePageFuncs(dir: string, funcs: (() => page.Page)[]) {
+    let w = new TemplateWriter(dir)
+    w.writePageFuncs(funcs)
+}
+
+export function writePages(dir: string, pages: page.Page[]) {
+    let w = new TemplateWriter(dir)
+    for (let p of pages) {
+        w.writePage(p)
+    }
+}
